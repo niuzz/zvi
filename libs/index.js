26f580e84ce185e2929918b02bb8a478ae3cc915
unknown <niuzz@hotmail.com>
2019-07-04 16:28:02 +0800

    chore: project structure

---
+++ libs/index.js
@@ -0,0 +1,25 @@
+import button from '&/components/button'
+
+const components = [
+  button
+]
+
+const install = function (Vue) {
+  components.map(component => {
+    Vue.component(component.name, component)
+  })
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export {
+  install,
+  button
+}
+
+export default {
+  install,
+  button
+}
